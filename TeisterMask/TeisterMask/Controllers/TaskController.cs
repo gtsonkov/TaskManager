8bdde143d1a23c46922c1fb8824a9ac1acd02dce
gtsonkov <gtsonkov@abv.bg>
2019-04-09 23:31:01 +0200

    DbConnection

---
+++ TeisterMask/TeisterMask/Controllers/TaskController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeisterMask.Data;
+
+namespace TeisterMask.Controllers
+{
+    public class TaskController : Controller
+    {
+        public IActionResult index()
+        {
+            using (var db = new TeisterMaskDbContex())
+            {
+                var tasks = db.Tasks.ToList();
+                return this.View(tasks);
+            }
+        }
+    }
+}
---
+++ TeisterMask/TeisterMask/Data/TeisterMaskDbContex.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TeisterMask.Models;
+
+namespace TeisterMask.Data
+{
+    public class TeisterMaskDbContex : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+        private const string DbConnString = @"Server=TS-PC\\SQLEXPRESS;Database=MasterTask_db;Integrated Security=True;";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=TS-PC\SQLEXPRESS;Database=MasterTaskdb;Integrated Security=True;");
+        }
+    }
+}
